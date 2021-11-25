72ee4cc96c6b90630a65ce2709a731d073d7d743
Valle1989 <fedevalle27@gmail.com>
2021-11-25 20:16:33 -0300

    Feature/21-model and repo Activity

--- src/main/java/com/alkemy/ong/model/Activity.java
+++ src/main/java/com/alkemy/ong/model/Activity.java
@@ -1,40 +1,51 @@
 package com.alkemy.ong.model;
 
 
-import com.sun.istack.NotNull;
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.SQLDelete;
-import org.hibernate.annotations.Where;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import javax.persistence.*;
-import java.util.Date;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
 
-@Data
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 @Entity
 @Table(name = "activities")
-@SQLDelete(sql = "UPDATE activities SET deleted = true WHERE id_post = ?")
-@Where(clause = "deleted = false")
+@SQLDelete(sql = "UPDATE activities SET deleted = true WHERE id = ?")
 public class Activity {
 
     @Id
     @GeneratedValue(strategy =  GenerationType.IDENTITY)
     private Long id;
 
-    @NotNull
+    @NotNull(message = "Name field cannot be null")
     private String name;
 
-    @NotNull
+    @NotNull(message = "Content field cannot be null")
     private String content;
 
-    @NotNull
+    @NotNull(message = "Image field cannot be null")
     private String image;
 
-    private Date timestamp;
+    @CreatedDate
+    @NotNull
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime dateCreation;
 
-    private boolean deleted;
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime dateUpdate;
 
-    public Activity(){
-
-    }
+    private boolean deleted = false;
 
 }
