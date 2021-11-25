b7201e3da6a54db5663ebf3286d81d904841d1ee
adrian010 <adriver010@gmail.com>
2021-11-25 13:28:00 -0300

    Correccion del pr

--- src/main/java/com/alkemy/ong/model/News.java
+++ src/main/java/com/alkemy/ong/model/News.java
@@ -1,34 +1,39 @@
 package com.alkemy.ong.model;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.sun.istack.NotNull;
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 import org.hibernate.annotations.SQLDelete;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
 @Entity
-@SQLDelete(sql = "UPDATE news enabled WHERE id = ?")
+@SQLDelete(sql = "UPDATE news SET enabled = false WHERE id = ?")
 @Table(name = "news")
 public class News {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotNull
-    private String name;
+    @NotNull(message = "name cannot be null")
+    private @NonNull String name;
 
-    @NotNull
-    private String content;
+    @NotNull(message = "content cannot be null")
+    private @NonNull String content;
 
-    @NotNull
-    private String image;
+    @NotNull(message = "image cannot be null")
+    private @NonNull String image;
+
+    @NotNull(message = "enabled cannot be null")
+    private @NonNull Boolean enabled = Boolean.TRUE;
 
     @CreatedDate
     @NotNull
@@ -40,14 +45,9 @@ public class News {
 
     @JsonBackReference
     @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-    @JoinColumn(name = "pk_categoryId")
-    private Category categoryId;
+    @JoinColumn(name = "categoryId")
+    private @NonNull Category categoryId;
 
-    public News(String name, String content, String image, Category categoryId){
-        this.name = name;
-        this.content = content;
-        this.image = image;
-        this.categoryId = categoryId;
-    }
+
 
 }
