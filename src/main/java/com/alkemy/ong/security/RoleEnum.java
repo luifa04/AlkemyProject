86251db5758126dd8c7ffd5a2523c7a29756e5ef
Gabriel Pilagatto <gabrielpilagatto@gmail.com>
2021-11-30 21:12:36 -0300

    modified RoleEnum

--- src/main/java/com/alkemy/ong/security/RoleEnum.java
+++ src/main/java/com/alkemy/ong/security/RoleEnum.java
@@ -1,5 +1,21 @@
 package com.alkemy.ong.security;
 
 public enum RoleEnum {
-    USER, ADMIN
+    ADMIN("ADMIN"),
+    USER("USER");
+
+    private final String name;
+    private static final String ROLE_ = "ROLE_";
+
+    RoleEnum(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getRoleName() {
+        return ROLE_ + name;
+    }
 }
