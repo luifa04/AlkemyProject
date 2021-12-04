b0b635127472c8d48b4e8fa5d1aa9894708559a8
FedeCaruso <fedecaruso418@gmail.com>
2021-12-04 15:36:26 -0300

    correcciones e implementacion de javaMailSender, falta aplicar MailException

--- src/main/java/com/alkemy/ong/service/EmailService.java
+++ src/main/java/com/alkemy/ong/service/EmailService.java
@@ -2,40 +2,42 @@
 package com.alkemy.ong.service;
 
 
-import com.sendgrid.Method;
-import com.sendgrid.Request;
-import com.sendgrid.Response;
-import com.sendgrid.SendGrid;
-import com.sendgrid.helpers.mail.Mail;
-import com.sendgrid.helpers.mail.objects.Content;
-import com.sendgrid.helpers.mail.objects.Email;
+import com.alkemy.ong.exception.EmailException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
 
-import java.io.IOException;
 
+@Service
 public class EmailService {
 
-    private static final SendGrid sendGrid = new SendGrid("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");
-    private static final Email from = new Email("oalkemy@gmail.com");
+    @Autowired
+    private JavaMailSender javaMailSender;
 
 
-    public void sendMail(String emailTo, String subject, String emailContent) throws IOException {
+    public void sendEmail(String to, String body, String subject) throws EmailException {
 
-        Email to = new Email(emailTo);
-        Content content = new Content("text/plain", emailContent);
-        Mail mail = new Mail(from, subject, to, content);
-
-        Request request = new Request();
-
-        try {
-            request.setMethod(Method.POST);
-            request.setEndpoint("mail/send");
-            request.setBody(mail.build());
-            Response response = sendGrid.api(request);
-            System.out.println(response.getStatusCode());
-            System.out.println(response.getBody());
-            System.out.println(response.getHeaders());
-        } catch (IOException ex) {
+        if (to.isEmpty()) {
+            throw new EmailException();
         }
+        if (body.isEmpty()) {
+            throw new EmailException();
+        }
+        if (subject.isEmpty()) {
+            throw new EmailException();
+        } else {
+            SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+            simpleMailMessage.setFrom("oalkemy@gmail.com");
+            simpleMailMessage.setTo(to);
+            simpleMailMessage.setSubject(subject);
+            simpleMailMessage.setText(body);
+            javaMailSender.send(simpleMailMessage);
+            System.out.println("Sent Email...");
+        }
+
+
     }
+
 }
 
