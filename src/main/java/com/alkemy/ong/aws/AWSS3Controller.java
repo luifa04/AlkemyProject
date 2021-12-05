1a2870daf55fb81b313e55291ef78d56803409fa
Gabriel Pilagatto <gabrielpilagatto@gmail.com>
2021-12-05 18:54:05 -0300

    added controller to upload images to aws s3

---
+++ src/main/java/com/alkemy/ong/aws/AWSS3Controller.java
@@ -0,0 +1,28 @@
+package com.alkemy.ong.aws;
+
+
+import com.amazonaws.SdkClientException;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/image")
+public class AWSS3Controller {
+
+        private final AWSS3ServiceImpl amazonClient;
+
+
+        @PostMapping("/upload")
+        @PreAuthorize("hasAnyRole(T(com.alkemy.ong.security.RoleEnum).ADMIN)")
+        public ResponseEntity<String> uploadImage(@RequestPart("file") MultipartFile file) throws IOException, SdkClientException {
+            return new ResponseEntity<>(amazonClient.uploadImage(file), HttpStatus.CREATED);
+        }
+
+}
