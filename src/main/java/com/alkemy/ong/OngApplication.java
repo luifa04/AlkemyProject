3d94a3c9368542b2af28a307f4a6eb6495cb0f51
Karen Hoffman <krenhoffman@gmail.com>
2021-12-06 12:17:46 -0300

    import correction

--- src/main/java/com/alkemy/ong/OngApplication.java
+++ src/main/java/com/alkemy/ong/OngApplication.java
@@ -1,12 +1,9 @@
 package com.alkemy.ong;
 
-import com.alkemy.ong.service.EmailService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
 
 @SpringBootApplication
 @EnableJpaAuditing
