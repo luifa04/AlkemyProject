e6660c68af1445aa21643a4023be46af4682c59d
Gabriel Pilagatto <gabrielpilagatto@gmail.com>
2021-12-07 18:29:13 -0300

    added Date Update in updateNews by ID

--- src/main/java/com/alkemy/ong/service/impl/NewsServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/impl/NewsServiceImpl.java
@@ -14,6 +14,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.Locale;
 
 @Service
@@ -39,6 +40,10 @@ public class NewsServiceImpl implements INewsService {
         Category categoryToUpdate = newsToUpdate.getCategoryId();
         Category category = categoryRepository.findById(news.getCategoryId()).orElseThrow(()->new NotFoundException(categoryNotFound));
         updateFields.updateIfNotBlankAndNotEqual(category, categoryToUpdate,newsToUpdate::setCategoryId, "category");
+
+        if (updateFields.isHasUpdate()){
+            newsToUpdate.setDateUpdate(LocalDateTime.now());
+        }
 
         return new ModelMapper()
                 .typeMap(News.class,NewsResponse.class)
