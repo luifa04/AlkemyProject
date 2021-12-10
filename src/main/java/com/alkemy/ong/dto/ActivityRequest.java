e02fc9dde06d69e371e1a599d04c46e1bc85df14
Karen Hoffman <krenhoffman@gmail.com>
2021-12-10 15:00:49 -0300

    creation of Activity Request and Response

---
+++ src/main/java/com/alkemy/ong/dto/ActivityRequest.java
@@ -0,0 +1,16 @@
+package com.alkemy.ong.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class ActivityRequest {
+
+    private String name;
+    private String content;
+    private String image;
+  
+}
---
+++ src/main/java/com/alkemy/ong/dto/ActivityResponse.java
@@ -0,0 +1,17 @@
+package com.alkemy.ong.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ActivityResponse {
+	
+    private String name;
+    private String content;
+    private String image;
+    
+
+}
