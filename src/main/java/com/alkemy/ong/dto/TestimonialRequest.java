65c7c8c7ec9dfe1ad1eff5689134dfb2a2bd3063
Gabriel Pilagatto <gabrielpilagatto@gmail.com>
2021-12-10 17:43:52 -0300

    change in validation

--- src/main/java/com/alkemy/ong/dto/TestimonialRequest.java
+++ src/main/java/com/alkemy/ong/dto/TestimonialRequest.java
@@ -4,16 +4,16 @@ import com.alkemy.ong.util.ImageExtension;
 import lombok.Getter;
 import org.hibernate.validator.constraints.URL;
 
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
 
 @Getter
 public class TestimonialRequest {
 
-    @NotNull(message = "name cannot be null")
+    @NotBlank(message = "name cannot be blank")
     private String name;
     @URL(message = "image must be an URL")
     @ImageExtension(message = "image extension not valid, must be JPG, JPEG or PNG")
     private String image;
-    @NotNull(message = "content cannot be null")
+    @NotBlank(message = "content cannot be blank")
     private String content;
 }
