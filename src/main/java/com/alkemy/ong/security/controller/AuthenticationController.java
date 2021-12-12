60b75b4eb6dcfb132658d4085bac16b374c8d39b
Jose Alejandro <62119845+alejandroitsoluciones@users.noreply.github.com>
2021-12-12 16:45:07 -0300

    Feature/62 send contact email (#65)
    * Applying hotfix/formatEmailWelcome changes
    
    * PreAutorize import is deleted
    
    * [IEmailService] is added to send email when registering contact
    
    * Annotation role @PreAuthorize detailed in ticket number 54 is fixed
    
    * Applied changes are reverted

--- src/main/java/com/alkemy/ong/security/controller/AuthenticationController.java
+++ src/main/java/com/alkemy/ong/security/controller/AuthenticationController.java
@@ -6,7 +6,6 @@ import com.alkemy.ong.security.service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import com.alkemy.ong.dto.UserRequest;
