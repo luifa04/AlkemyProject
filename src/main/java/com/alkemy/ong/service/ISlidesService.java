1995188b23a8cbe9e03c739abab45417947ca748
Karen Hoffman <krenhoffman@gmail.com>
2021-12-12 12:22:04 -0300

    endpoint showing the attributes of a slide

--- src/main/java/com/alkemy/ong/service/ISlidesService.java
+++ src/main/java/com/alkemy/ong/service/ISlidesService.java
@@ -1,14 +1,15 @@
 package com.alkemy.ong.service;
 
 import com.alkemy.ong.dto.SlideRequest;
+import com.alkemy.ong.dto.SlideResponse;
 import com.alkemy.ong.model.Slide;
 import org.springframework.http.ResponseEntity;
 
-import java.io.IOException;
 
 public interface ISlidesService {
 
     Slide addSlide(SlideRequest slide) throws Exception;
     public ResponseEntity<?> delete(Long id);
+    public SlideResponse detail(Long id);
 
 }
