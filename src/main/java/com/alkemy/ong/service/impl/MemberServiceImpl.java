4c6354493417466ca449220eadb95bf747625385
Jose Alejandro <alejandroitsoluciones@gmail.com>
2021-12-14 12:36:33 -0300

    Impl service class is created for Member with List method

---
+++ src/main/java/com/alkemy/ong/service/impl/MemberServiceImpl.java
@@ -0,0 +1,34 @@
+package com.alkemy.ong.service.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.alkemy.ong.dto.MemberResponse;
+import com.alkemy.ong.model.Member;
+import com.alkemy.ong.repository.MemberRepository;
+import com.alkemy.ong.service.IMemberService;
+import com.alkemy.ong.mapper.MemberMapper;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class MemberServiceImpl implements IMemberService{
+
+	private final MemberRepository memberRepository;
+	private final MemberMapper memberMapper;
+	
+	@Override
+	public List<MemberResponse> getAllMembers(){
+		List<Member> listMember = memberRepository.findAll();
+		List<MemberResponse> listMemberResponse = new ArrayList<>();
+		listMember.stream().forEach(member -> {
+			MemberResponse memberResponse = memberMapper.memberModel2DTO(member);
+			listMemberResponse.add(memberResponse);
+		});
+		return listMemberResponse;
+	}
+	
+}
