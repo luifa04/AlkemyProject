ba7ff0b88caf242a33aff54934ca4f5c7379a465
Jose Alejandro <alejandroitsoluciones@gmail.com>
2021-12-14 13:51:45 -0300

    Exception message of empty list of members is added

--- src/main/java/com/alkemy/ong/service/impl/MemberServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/impl/MemberServiceImpl.java
@@ -2,10 +2,13 @@ package com.alkemy.ong.service.impl;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
 
 import com.alkemy.ong.dto.MemberResponse;
+import com.alkemy.ong.exception.NotFoundException;
 import com.alkemy.ong.model.Member;
 import com.alkemy.ong.repository.MemberRepository;
 import com.alkemy.ong.service.IMemberService;
@@ -19,15 +22,20 @@ public class MemberServiceImpl implements IMemberService{
 
 	private final MemberRepository memberRepository;
 	private final MemberMapper memberMapper;
+	private final MessageSource messageSource;
 	
 	@Override
 	public List<MemberResponse> getAllMembers(){
+		String memberListIsEmpty = messageSource.getMessage("member.listEmpty", null, Locale.US);
 		List<Member> listMember = memberRepository.findAll();
 		List<MemberResponse> listMemberResponse = new ArrayList<>();
 		listMember.stream().forEach(member -> {
 			MemberResponse memberResponse = memberMapper.memberModel2DTO(member);
 			listMemberResponse.add(memberResponse);
 		});
+        if(listMemberResponse.isEmpty()){
+            throw new NotFoundException(memberListIsEmpty);
+        }		
 		return listMemberResponse;
 	}
 	
