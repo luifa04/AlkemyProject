ee5bcf3bcb8ea2493be814c60c3f74f4b99ce765
Jose Alejandro <62119845+alejandroitsoluciones@users.noreply.github.com>
2021-12-16 17:37:14 -0300

    Feature/80 add paging to category (#78)
    * Mapping functions are moved to [CategoryMapper]
    
    * [CategoryMapper] is created with manual mapping methods
    
    * Fixed method name
    
    * Fixed variable name
    
    * Unnecessary import was deleted
    
    * Spring HATEOAS is added
    
    * CategoryAssembler is created that extends [RepresentationModelAssemblers]
    
    * Inheritance from [RepresentationModel] base class added for DTOs to collect bindings
    
    * A method that returns pagination is added in the interface
    
    * Paging method is implemented and constant with page size is added
    
    * Exception message is added
    
    * Paging is implemented in the controller
    
    * User role is fixed
    
    * Constant is added with User or Admin roles with SpEL
    
    * Fix role in @PreAutorize annotation

--- src/main/java/com/alkemy/ong/service/ICategoryService.java
+++ src/main/java/com/alkemy/ong/service/ICategoryService.java
@@ -8,12 +8,13 @@ package com.alkemy.ong.service;
 
 import com.alkemy.ong.dto.CategoryByNameDto;
 import javax.validation.Valid;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import com.alkemy.ong.dto.CategoryDto;
 import com.alkemy.ong.dto.CategoryRequestUpdate;
-
-
-
+import com.alkemy.ong.model.Category;
 
 import java.util.List;
 
@@ -21,7 +22,7 @@ import java.util.List;
 public interface ICategoryService {
 
 
-    public List<CategoryByNameDto> findByName();
+    public List<CategoryByNameDto> findAllByName();
 
     public CategoryDto createCategory(@Valid CategoryRequestUpdate category);
 
@@ -30,6 +31,8 @@ public interface ICategoryService {
     CategoryDto findById(Long id);
     
 	CategoryDto updateCategory(@Valid CategoryRequestUpdate category, Long id);
+
+	Page<Category> readAllCategoriesByName(Pageable pageable, int page);
 
 
 }
