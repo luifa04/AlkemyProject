c6a82a4d3248ecd366ccea00c30d53a2db60e1f0
adrian010 <adriver010@gmail.com>
2021-12-16 16:20:46 -0300

    Add pagination news

--- src/main/java/com/alkemy/ong/dto/NewsResponse.java
+++ src/main/java/com/alkemy/ong/dto/NewsResponse.java
@@ -1,13 +1,14 @@
 package com.alkemy.ong.dto;
 
 import lombok.*;
+import org.springframework.hateoas.RepresentationModel;
 
 @Getter
 @Setter
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
-public class NewsResponse {
+public class NewsResponse extends RepresentationModel<NewsResponse> {
 
     private Long id;
     private String name;
