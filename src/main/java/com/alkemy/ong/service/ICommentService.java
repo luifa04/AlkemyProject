bd2ff6392a73502c79a26f081e944945b994a05f
mayorgaclara <mayorgaclara@gmail.com>
2021-12-18 15:36:00 -0300

    Feature/70-ListComments

--- src/main/java/com/alkemy/ong/service/ICommentService.java
+++ src/main/java/com/alkemy/ong/service/ICommentService.java
@@ -1,10 +1,15 @@
 package com.alkemy.ong.service;
 
 import com.alkemy.ong.dto.CommentRequest;
+import com.alkemy.ong.dto.CommentResponseList;
+import com.alkemy.ong.dto.ContactResponseDto;
 import com.alkemy.ong.model.Comment;
+
+import java.util.List;
 
 public interface ICommentService {
 
     Comment addComment(CommentRequest commentRequest);
+    List<CommentResponseList> getAll();
 
 }
--- src/main/java/com/alkemy/ong/service/impl/CommentServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/impl/CommentServiceImpl.java
@@ -1,21 +1,30 @@
 package com.alkemy.ong.service.impl;
 
 import com.alkemy.ong.dto.CommentRequest;
+import com.alkemy.ong.dto.CommentResponseList;
+import com.alkemy.ong.exception.EmptyDataException;
+import com.alkemy.ong.mapper.CommentsMapper;
 import com.alkemy.ong.model.Comment;
 import com.alkemy.ong.model.News;
 import com.alkemy.ong.model.User;
 import com.alkemy.ong.repository.CommentRepository;
 import com.alkemy.ong.service.ICommentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Locale;
 
 @Service
 @RequiredArgsConstructor
 public class CommentServiceImpl implements ICommentService {
 
     private final CommentRepository commentRepository;
+    private final CommentsMapper commentsMapper;
     private final UserServiceImpl userService;
     private final NewsServiceImpl newsService;
+    private final MessageSource messageSource;
 
     @Override
     public Comment addComment(CommentRequest commentRequest) {
@@ -30,5 +39,15 @@ public class CommentServiceImpl implements ICommentService {
 
         return commentRepository.save(comment);
 
+    }
+
+    public List<CommentResponseList> getAll() {
+        String commentListIsEmpty = messageSource.getMessage("comments.listEmpty", null, Locale.US);
+        if(commentRepository.findAll().isEmpty()){
+            throw new EmptyDataException(commentListIsEmpty);
+        }
+        List<Comment> entities = commentRepository.findAll();
+        List<CommentResponseList> result = commentsMapper.commentModelList2DTOList(entities);
+        return result;
     }
 }
