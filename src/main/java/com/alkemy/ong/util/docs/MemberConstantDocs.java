c87e4d7977b7a51fc9b157bf3a7f10ec2d9ed489
Karen Hoffman <krenhoffman@gmail.com>
2021-12-23 20:44:42 -0300

    add 'MemberConstantDocs'

---
+++ src/main/java/com/alkemy/ong/util/docs/MemberConstantDocs.java
@@ -0,0 +1,42 @@
+package com.alkemy.ong.util.docs;
+
+public class MemberConstantDocs {
+	
+	    String MEMBER = "Member documentation";
+
+	    String MEMBER_DTO_ID = "Member id";
+	    String MEMBER_DTO_NAME = "Members's name";
+	    String MEMBER_DTO_DESCRIPTION = "Member´s description";
+	    String MEMBER_DTO_IMAGE = "Member´s image";
+	    String MEMBER_DTO_DATE_CREATION = "Member's date creation";
+	    String MEMBER_DTO_DATE_UPDATE = "Member´s date update";
+
+	    String MEMBER_MEMBERREQUESTUPDATE_NAME = "Enter a name for the member";
+	    String MEMBER_MEMBERREQUESTUPDATE_DESCRIPTION = "Enter the description of the member";
+	    String MEMBER_MEMBERREQUESTUPDATE_IMAGE = "Enter a valid url of an image";
+
+	   /* String CATEGORY_FIND_BY_ID = "Returns a category through id parameter";
+	    String CATEGORY_GET_200_OK = "Category found";
+	    String CATEGORY_GET_404_NOT_FOUND = "Page number not found";
+
+	    String CATEGORY_FIND_ALL_CATEGORIES_BY_NAME = "Returns a paginated list of categories";
+	    String CATEGORY_GET_PARAM_ID = "Enter an existing id to search category";
+	    String CATEGORY_GET_PARAM_PAGE_NUMBER = "Enter an existing number page to search category";*/
+
+	    String MEMBER_CREATE = "Create new member";
+	    String MEMBER_CREATED = "The member has been created";
+	    String MEMBER_CREATED_PARAM_MEMBER_REQUEST = "Fill in the required fields to save a member";
+
+	    String MEMBER_UPDATE = "Update an existing member";
+	    String MEMBER_UPDATE_OK = "The member has been updated successfully";
+	    String MEMBER_UPDATE_PARAM_ID = "Enter an existing id to update member";
+	    String MEMBER_UPDATE_PARAM_MEMBER_REQUEST = "Fill in the required fields to update a member";
+
+	   /* String CATEGORY_DELETE = "Delete a category";
+	    String CATEGORY_DELETE_OK = "The category has been removed";
+	    String CATEGORY_DELETE_PARAM_ID = "Enter an existing id to remove category";
+
+	    String CATEGORY_404_NOT_FOUND = "Cannot find any categories";*/
+
+
+}
