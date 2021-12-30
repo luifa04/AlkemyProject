2088569a641d11eff91e9e628c9dc95dc520f8a5
Karen Hoffman <krenhoffman@gmail.com>
2021-12-30 18:43:12 -0300

    upgrade

--- src/test/java/com/alkemy/ong/integration/contact/ContactCreateGeneralTest.java
+++ src/test/java/com/alkemy/ong/integration/contact/ContactCreateGeneralTest.java
@@ -1,23 +1,27 @@
 package com.alkemy.ong.integration.contact;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.isA;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.mockito.Mockito;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import com.alkemy.ong.common.BaseContactTest;
 import com.alkemy.ong.dto.ContactRequestDto;
+import com.alkemy.ong.dto.ContactResponseDto;
 import com.alkemy.ong.model.Contact;
 import com.alkemy.ong.repository.ContactRepository;
 import com.alkemy.ong.security.RoleEnum;
 
 public class ContactCreateGeneralTest extends BaseContactTest {
 	
-	
+	 private final String PATH = "/contacts/";
 	 private ContactRepository contactRepository;
 	 private ContactRequestDto contactRequest;
 	 private RoleEnum roleEnum;
@@ -30,7 +34,34 @@ public class ContactCreateGeneralTest extends BaseContactTest {
         Mockito.when(contactRepository.save(isA(Contact.class))).thenReturn(generateContact());
         return contact;
     }
+    
+    @Test
+    public void ReturnUnauthorizedIfUserIsNotAdmin() {
 
+        login(RoleEnum.USER.getRoleName());
+        ResponseEntity<Object> response = testRestTemplate.exchange(createURLWithPort(PATH),
+                HttpMethod.POST, new HttpEntity<>(contactRequest, headers), Object.class);
+        assertEquals(response.getStatusCode(), HttpStatus.UNAUTHORIZED);
+    }
+    
+
+    @Before
+    public void AdminLogin() {
+        login(RoleEnum.ADMIN.getRoleName());
+    }
+    
+    @Test
+    public void ReturnBadRequestIfAnyAttributeIsNull() {
+        contactRequest.setName("");
+        contactRequest.setEmail("");
+        contactRequest.setMessage("");
+
+        ResponseEntity<ContactResponseDto> response = testRestTemplate.exchange(createURLWithPort(PATH),
+                HttpMethod.POST, new HttpEntity<>(contactRequest, headers), ContactResponseDto.class);
+
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    }
+    
     @Test
     public void CreateContactSuccess() {
 
@@ -45,24 +76,23 @@ public class ContactCreateGeneralTest extends BaseContactTest {
         ResponseEntity<?> response =
                 testRestTemplate.exchange(createURLWithPort("/contacts"), HttpMethod.POST, new HttpEntity<>(contactRequest, headers), ContactRequestDto.class); 
         
-        
+        assertEquals(response.getStatusCode(), HttpStatus.CREATED);
     }
    
                 	
     @Test
     public void CreateContactFailedBecauseName() {
-
-        Contact contact = createContact();
-        contact.setId(1L);
-        contact.setName("");
-        contact.setEmail("https://somosmas.jpg");
+    	
+    	ContactRequestDto contactRequest = exampleContactRequest();
+    	
+        contactRequest.setId(1L);
+        contactRequest.setName("");
+        contactRequest.setEmail("https://somosmas.jpg");
         contact.setMessage("Mensaje de prueba");
 
         Mockito.when(contactRepository.save(isA(Contact.class))).thenReturn(generateContact());
 
-        login(RoleEnum.ADMIN.getRoleName());
-
-        ContactRequestDto contactRequest = exampleContactRequest(); //por qué esta linea?
+        //por qué esta linea?
         
     }
     
