20113e0907e8fe386344fd59e9b66bb7c102d0ae
Mateo Lopez <mateolopez27.11@gmail.com>
2024-01-19 10:45:31 -0300

    last changes

--- src/main/java/com/alkemy/ong/config/SwaggerConfig.java
+++ src/main/java/com/alkemy/ong/config/SwaggerConfig.java
@@ -11,8 +11,10 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.service.ApiKey;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Configuration
@@ -20,16 +22,21 @@ import java.util.List;
 public class SwaggerConfig {
 
     @Bean
-    public Docket ApiDocket(){
+    public Docket apiDocket() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .securitySchemes(Arrays.asList(apiKey()));
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("Token Access", "Authorization", "header");
     }
 
     @Bean
-    public LinkDiscoverers Discoverers(){
+    public LinkDiscoverers discoverers() {
         List<LinkDiscoverer> plugins = new ArrayList<>();
         plugins.add(new CollectionJsonLinkDiscoverer());
 
